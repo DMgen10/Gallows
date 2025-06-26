47c7e01f2aec015886bccfe93f2322dab890a578
dmitry <dmitryed6263@gmail.com>
2025-06-26 21:38:34 +0400

    add WordGenerator

--- src/main/java/dev/dmitry/WordGenerator.java
+++ src/main/java/dev/dmitry/WordGenerator.java
@@ -1,13 +1,26 @@
 package dev.dmitry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class WordGenerator {
 
+    private WordParser wordParser;
     private Random random = new Random();
-    private List<String> listOfWord = new ArrayList<>();    // сюда буду класть все слова
-
     private static String randomWord;                             // это слово буду отдавать на отгадывание
+    static private int index;
+
+    public WordGenerator(WordParser wordParser) {
+        this.wordParser = wordParser;
+    }
 
     public void generate(){
-        randomWord = "";
+        index = random.nextInt(wordParser.getWords().size() );
+        randomWord = wordParser.getWords().get(index);
+    }
+
+    public String getRandomWord(){
+        return randomWord;
     }
 }
