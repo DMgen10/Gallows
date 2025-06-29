40090130e9106ab9c296ed80925a76c28a0c146d
dmitry <dmitryed6263@gmail.com>
2025-06-29 21:55:09 +0400

    ну тут в принципе всё

--- src/main/java/dev/dmitry/InputService.java
+++ src/main/java/dev/dmitry/InputService.java
@@ -1,5 +1,4 @@
 package dev.dmitry;
-
 import java.util.Scanner;
 
 public class InputService {
@@ -7,24 +6,50 @@ public class InputService {
     // Проверки введенного символа: 1.что бы он был один 2. все символы будут притянуты к нижнему регистру 3. русский язык!
     // 4. это должна быть строка
 
-    static Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
 
-    public static String input(){
-        System.out.println("введите любой символ");
-        String letter = scanner.nextLine();
-        System.out.println("вы ввели " + letter);
-        return letter;
+    public InputService(){
+        this.scanner = new Scanner(System.in);
     }
 
-    public static boolean checkForNumberOfCharacters(String letter){
-            if (letter.length() == 1){
-                System.out.println("1 символ - ок пропускаем");
-                return true;
-            } else {
-                System.out.println("нихуя не ок");
-                return false;
+    public String readInput(){
+
+        while (true){
+            System.out.println("Введите одну русскую букву");
+            String letter = scanner.nextLine().toLowerCase();
+
+            if (!isSingleCharacter(letter)){
+                System.out.println("Введите ровно один символ");
+                continue;
+            }
+
+            if (!isLetter(letter)){
+                System.out.println("Введенный символ не является буквой");
+                continue;
+            }
+
+            if (!isRussianLetter(letter)){
+                System.out.println("Вы ввели не Русскую букву");
+                continue;
+            }
+
+            System.out.println("Вы ввели:" + letter);
+            return letter;
         }
     }
+
+    private boolean isSingleCharacter(String letter){        // проверка что символ ОДИН!!!!
+        return letter.length() == 1;
+    }
+
+    private boolean isLetter(String letter){        // проверка на то что строка является строкой а не цифрой и т.д...
+        return Character.isLetter(letter.charAt(0));
+    }
+
+    private boolean isRussianLetter(String letter){       // проверяем на рус. язык
+        return letter.matches("[А-Яа-яЁё]");
+    }
+
 
 
 }
