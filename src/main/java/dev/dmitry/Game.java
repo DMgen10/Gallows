e7b5ea54acd29b56999da8df16c1774f6c86b6d4
dmitry <dmitryed6263@gmail.com>
2025-06-30 22:27:08 +0400

    чуть допилил

--- src/main/java/dev/dmitry/Game.java
+++ src/main/java/dev/dmitry/Game.java
@@ -1,5 +1,8 @@
 package dev.dmitry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     /*
@@ -18,24 +21,44 @@ public class Game {
     private int numberOfErrors = 0;
     private final int maxErrors = 6;
     private final List<String> usedLetters = new ArrayList<>();
-
-
+    private final List<String> wrongLetters = new ArrayList<>();
 
     private void doPlay(){
         parser.loadWords();
         generator.generate();
         hiddenWord = generator.getRandomWord();
-        modifier = new VisibilityModifier(generator.getRandomWord());
+        modifier = new VisibilityModifier(hiddenWord);
     }
 
     public void play(){
         doPlay();
+
         while (numberOfErrors < maxErrors && !modifier.isFullyRevealed()){
             System.out.println("Слово " + modifier.getMaskedWord());
             System.out.println("Осталось попыток: " + (maxErrors - numberOfErrors));
-
             String input = inputService.readInput();
+            System.out.println("Ошибочные буквы: " + String.join(", ", wrongLetters));
+            if (usedLetters.contains(input)) {
+                System.out.println("Вы уже вводили букву '" + input + "'. Попробуйте другую.");
+                continue;
+            }
 
+            usedLetters.add(input);
+            boolean isFound = modifier.updateState(input);
+
+            if (!isFound) {
+                numberOfErrors++;
+                wrongLetters.add(input);
+                System.out.println("В загаданном слове буква " + input + " отсутствует ");
+            } else {
+                System.out.println("Ты прав, буква " + input + "есть в слове!");
+            }
+            System.out.println();
+        }
+        if (modifier.isFullyRevealed()){
+            System.out.println("Вы выиграли, загаданным словом было: " + hiddenWord);
+        } else {
+            System.out.println("Вы проиграли, загаданным словом было: " + hiddenWord);
         }
     }
 }
