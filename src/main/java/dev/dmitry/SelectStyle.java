dbfd1f9eb7e4b18a2a6dabbf13bd77a23ba0eade
dmitry <dmitryed6263@gmail.com>
2025-07-02 23:05:15 +0400

    класс для выбора отображения

---
+++ src/main/java/dev/dmitry/SelectStyle.java
@@ -0,0 +1,23 @@
+package dev.dmitry;
+
+public class SelectStyle {
+
+    private final InputService service = new InputService();
+
+    public boolean selectStyle() {
+        System.out.println("выберите стиль отображения:");
+        System.out.println("1 - классическая виселица");
+        System.out.println("2 - емодзи");
+
+        while (true) {
+            String choice = service.readMenuInput();
+            if (choice.equals("1")) {
+                return false;
+            } else if (choice.equals("2")) {
+                return true;
+            } else {
+                System.out.println("Вы вводите что-то иное...");
+            }
+        }
+    }
+}
